00576ed0296c3bbbd0d3ee01980c6b6e28ddf773
Michael Weigle <michael.weigle@gmail.com>
2010-06-24 17:14:16 -0400

    Stripped deprecated code and replaced with an instance of a FileManipulator
    Signed-off-by: Michael Weigle <michael.weigle@gmail.com>

--- src/dpsssuperprocessor9000/Main.java
+++ src/dpsssuperprocessor9000/Main.java
@@ -1,10 +1,8 @@
 package dpsssuperprocessor9000;
 
-import java.io.*;
-
 /**
  * Digital Production Services' Super Processor 9000
- * @version 2010.0623
+ * @version 2010.0624
  * @author Michael Weigle <michael.weigle@gmail.com>
  */
 public class Main
@@ -14,36 +12,7 @@ public class Main
      */
     public static void main( String[] args )
     {
-        String firstFileName = getFileName( "first" );
-        String lastFileName = getFileName( "last" );
-
-        // Tokenize into relevant fields
-    }
-
-    /**
-     * Requests an absolute filename from the user via the console.
-     *
-     * @param target description of the type of file being requested (e.g. "first" or "last")
-     * @return a String containing the absolute path indicated by the user
-     */
-    private static String getFileName( String target )
-    {
-        System.out.println( "Please enter the absolute filename for the " + target + " file" );
-
-        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-        String targetName = null;
-
-        try
-        {
-            targetName = br.readLine();
-        }
-        catch( IOException ioe )
-        {
-            System.out.println( "IO Error when reading " + target + "file." );
-            targetName = null;
-        }
-
-        return targetName;
+        FileManipulator handler = new FileManipulator();
     }
 
 }
