c979fee433208a756fe2bd899ad00b346c5e858a
Michael Weigle <michael.weigle@gmail.com>
2010-07-01 14:53:36 -0400

    Added comments and descriptions. Put in writing encoding assumptions (TXT - windows-1252, XML - UTF-8, * - binary)
    Signed-off-by: Michael Weigle <michael.weigle@gmail.com>

--- src/dpsssuperprocessor9000/Job.java
+++ src/dpsssuperprocessor9000/Job.java
@@ -1,8 +1,6 @@
 
 package dpsssuperprocessor9000;
 
-import java.io.*;
-
 /**
  * @desc An implementing object contains instructions on how to process a single
  * file given to it.
@@ -10,7 +8,33 @@ import java.io.*;
  */
 public interface Job
 {
+    /**
+     * Contains instructions for how to manipulate a single file. Assumes TXT
+     * files are windows-1252 encoded, XML files are UTF-8 encoded, and other
+     * file types are binary encoded.
+     * 
+     * @param handler the FileManipulator instance containing token information
+     * @return true if success, false is failure
+     */
     boolean process( FileManipulator handler );
+
+    /**
+     * Initializes an input stream according to what's needed. Assumes TXT files
+     * are windows-1252 encoded, XML files are UTF-8 encoded, and other file
+     * types are binary encoded.
+     *
+     * @param handler the FileManipulator instance containing token information
+     * @return true if success, false is failure
+     */
     boolean initInStream( FileManipulator handler );
+    
+    /**
+     * Initializes an output stream according to what's needed. Assumes TXT
+     * files are windows-1252 encoded, XML files are UTF-8 encoded, and other
+     * file types are binary encoded.
+     * 
+     * @param handler the FileManipulator instance containing token information
+     * @return true if success, false is failure
+     */
     boolean initOutStream( FileManipulator handler );
 }
